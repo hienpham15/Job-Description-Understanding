6920110f18401539e6f283d205d20e20eb331fb1
hienpham15 <phamxuanhien15@gmail.com>
2022-06-28 17:19:37 +0200

    Spider only crawl 1 page

--- scrapBot/jobs/spiders/indeed.py
+++ scrapBot/jobs/spiders/indeed.py
@@ -1,14 +1,17 @@
 import scrapy
 import logging
-from bs4 import BeautifulSoup
+import time
 from itertools import product
-from selenium import webdriver
 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-job_titles = ["Data Engineer", "Data Scientist", "Data Analyst", "Machine Learning Engineer"]
+job_titles = ["Data Engineer",
+              "Data Scientist",
+              "Data Analyst",
+              "Machine Learning Engineer"]
 regions = ["Île-de-France"]
 villes = ["Fontenay-aux-Roses"]
 urls = []
@@ -16,114 +19,81 @@ urls = []
 for (job_title, region, ville) in product(job_titles, regions, villes):
     urls.append(f"https://fr.indeed.com/emplois?q={' '.join(job_title.split())}&l={region}&rbl={ville}.html")
 
+
 class IndeedSpider(scrapy.Spider):
     name = 'indeed'
     allowed_domains = ['indeed.com']
     start_urls = urls
-# =============================================================================
-# 
-#     def parse_jd(self, response, **posting):
-#         soup = BeautifulSoup(response.text, features="lxml")
-#         jd = soup.find("div", {"id": "jobDescriptionText"}).get_text()
-#         url = response.url
-#         posting.update({"job_description": jd, "url": url}) 
-#         yield posting
-# 
-#     def parse(self, response):
-#         soup = BeautifulSoup(response.text, features="lxml")
-# 
-#         listings = soup.find_all("a", {"class": "tapItem"})
-#         for listing in listings:
-#             job_title = listing.find("h2", {"class": "jobTitle"}).get_text().strip()
-#             #logging.log("Job Title:", job_title)
-#             summary = listing.find("div", {"class": "job-snippet"}).get_text().strip()    # strip newlines
-#             #logging.log("Job Summary:", summary)
-#             company = listing.find("span", {"class": "companyName"}).get_text().strip()
-#             location = listing.find("div", {"class": "companyLocation"}).get_text().strip()
-# 
-#             posting = {"job_title": job_title, "summary": summary, "company": company, "location": location}
-#             jd_page = listing.get("href")
-#             
-#             print(posting)
-#             if jd_page is not None:
-#                 yield response.follow(jd_page, callback=self.parse_jd, cb_kwargs=posting)
-# =============================================================================
+
     def __init__(self):
         self.options = webdriver.FirefoxOptions()
-        self.options.add_argument("headless")
-        self.driver = webdriver.Firefox(desired_capabilities=self.options.to_capabilities())
-        
-    
+        self.options.add_argument("--headless")
+        self.page_driver = webdriver.Firefox(desired_capabilities=self.options.to_capabilities())
+
+
     def parse_jd(self, jd_link, **posting):
         driver_cb = webdriver.Firefox(desired_capabilities=self.options.to_capabilities())
         driver_cb.get(jd_link.url)
-        print("jd link:", jd_link.url)
+        # print("jd link:", jd_link.url)
+
         driver_cb.implicitly_wait(10)
-        
         wait = WebDriverWait(driver_cb, 5)
+        wait.until(EC.presence_of_element_located((By.ID, "jobDescriptionText")))
+
         job_description = driver_cb.find_element_by_id("jobDescriptionText")
-        print('Job Description: ', job_description.text)
-        #driver_cb.quit()
-        posting.update({"job description": job_description.text})
-        print("posting_final:", posting)
+        posting_duration = driver_cb.find_element_by_css_selector(".jobsearch-HiringInsights-entry--text")
+        # print('Job Description: ', job_description.text)
+        posting.update({"job description": job_description.text,
+                        "posting duration": posting_duration.text})
+        # print("posting_final:", posting)
         yield posting
         driver_cb.quit()
-        
-        
+
+
     def parse(self, response):
         # Use headless option and Firefox browser as the driver
-# =============================================================================
-#         options = webdriver.FirefoxOptions()
-#         options.add_argument("headless")
-#         driver = webdriver.Firefox(desired_capabilities=options.to_capabilities())
-# =============================================================================
-          
-        self.driver.get("https://fr.indeed.com/jobs?q=Data%20Engineer&l=%C3%8Ele-de-France&rbl=Fontenay-aux-Roses.html&start=150&vjk=42ca2e270d024eee")
-        
-        self.driver.implicitly_wait(10)
-        
-        wait = WebDriverWait(self.driver, 5)
-        
-        job_titles = self.driver.find_elements_by_class_name("jobTitle")
-        companies = self.driver.find_elements_by_class_name("companyName")
-        #jd_links = self.driver.find_elements_by_class_name("jcs-JobTitle")
-        # class="jcs-JobTitle" with attribute href => follow href to get detail job Description
-        #descriptions = driver.find_elements_by_id("jobDescriptionText")
-        
-        listings = self.driver.find_elements_by_class_name("tapItem")
-        
+        driver = webdriver.Firefox(desired_capabilities=self.options.to_capabilities())
+
+        # driver.get("https://fr.indeed.com/jobs?q=Data%20Engineer&l=%C3%8Ele-de-France&rbl=Fontenay-aux-Roses.html&start=150&vjk=42ca2e270d024eee")
+        driver.get("https://fr.indeed.com/emplois?q=Data Engineer&l=Île-de-France&rbl=Fontenay-aux-Roses.html")
+
+        driver.implicitly_wait(10)
+        wait = WebDriverWait(driver, 5)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "tapItem")))
+
+
+        listings = driver.find_elements_by_class_name("tapItem")
+
         for item in listings:
-            print("item:", item)
             job_title = item.find_element_by_class_name("jobTitle")
             print("Job Title:", job_title.text)
             company = item.find_element_by_class_name("companyName")
             location = item.find_element_by_class_name("companyLocation")
             summary = item.find_element_by_class_name("job-snippet")
-            
-            posting = {"job_title": job_title.text, "summary": summary.text, "company": company.text, "location": location.text}
-            print("posting:", posting)
+
             jd_page = item.find_element_by_class_name("jcs-JobTitle")
+            job_id = jd_page.get_attribute("data-jk")
+            posting = {"job id": job_id,
+                       "job_title": job_title.text,
+                       "company": company.text,
+                       "location": location.text,
+                       "summary": summary.text}
+
             if jd_page is not None:
                 jd_link = jd_page.get_attribute("href")
-                print("Call back using href link...")
-                #print("jd link:", jd_link.url)
-                yield response.follow(jd_link, callback=self.parse_jd, cb_kwargs=posting)
-                
-# =============================================================================
-#         for i in range(len(job_titles)):
-#             
-#             yield {
-#                 "Company" : companies[i].text,
-#                 "Job Title" : job_titles[i].text,}
-#                 "Job Description" : self.parse_jd(jd_links[i].get_attribute('href'))}
-# =============================================================================
-            
-        self.driver.quit()
-        
+                yield response.follow(jd_link,
+                                      callback=self.parse_jd,
+                                      cb_kwargs=posting)
 
-# =============================================================================
-#         next_page = soup.find("a", {"aria-label":  "Suivant"}).get("href")
-#         if next_page is not None:
-#             next_page = response.urljoin(next_page)
-#             yield scrapy.Request(next_page, callback=self.parse)
-# =============================================================================
+        time.sleep(5)
+
+        xp_next = driver.find_element_by_xpath("//a[@aria-label='Suivant']")
+        if xp_next is not None:
+            current_page = driver.find_element_by_xpath('//b[@aria-current="true"]')
+            print("Page: ", current_page.text)
+            next_page_href = xp_next.get_attribute("href")
+            print("Next page links:", next_page_href)
+            next_page_url = response.urljoin(next_page_href)
+            yield scrapy.http.Request(url=next_page_url, callback=self.parse)
+
+        driver.quit()
