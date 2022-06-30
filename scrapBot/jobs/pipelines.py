907ea4b5a30017066564c16d7d84fc9f51baba35
hienpham15 <phamxuanhien15@gmail.com>
2022-06-30 23:15:44 +0200

    Added MongoDB connector

--- scrapBot/jobs/pipelines.py
+++ scrapBot/jobs/pipelines.py
@@ -7,6 +7,15 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import pymongo
+from scrapy.conf import settings
+
+class MongoDBPipeline(object):
+    def __init__(self):
+        connection = pymongo.MongoClient(settings['MONGODB_SERVER'],
+                                         settings['MONGODB_PORT'])
+        db = connection([settings['MONGODB_DB']])
+        self.collection = db[settings['MONGODB_COLLECTION']]
 
 class JobsPipeline:
     def process_item(self, item, spider):
