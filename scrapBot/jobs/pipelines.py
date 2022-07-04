bc047ecce80bde098340fa78e94fca482fefb3a1
hienpham15 <phamxuanhien15@gmail.com>
2022-07-04 18:00:28 +0200

    Added option to connect to remote Mongodb

--- scrapBot/jobs/pipelines.py
+++ scrapBot/jobs/pipelines.py
@@ -9,8 +9,8 @@ from itemadapter import ItemAdapter
 
 import pymongo
 import sys
-#from scrapy.conf import settings
-#from scrapy import log
+# from scrapy.conf import settings
+# from scrapy import log
 
 # =============================================================================
 # class MongoDBPipeline(object):
@@ -19,7 +19,7 @@ import sys
 #                                          settings['MONGODB_PORT'])
 #         db = connection([settings['MONGODB_DB']])
 #         self.collection = db[settings['MONGODB_COLLECTION']]
-#         
+#
 #     def process_item(self, posting, spider):
 #         # self.collection.update()
 #         log.msg("Crawled 3 indeed pages for DE offers",
@@ -34,28 +34,38 @@ import sys
 #         return item
 # =============================================================================
 
+
 class MongoDBPipeline:
     collection = 'jds_indeed'
-    
+    user_name = 'HienPHAM'
+    password = 'myfamily01060204'
+    remote_uri = 'mongodb+srv://' + user_name + ':' + password + \
+                 '@jdu-project.nkmrf.mongodb.net/'
+    init_db = 'jobs_indeed'
+
     def __init__(self, mongodb_uri, mongodb_db):
         self.mongodb_uri = mongodb_uri
         self.mongodb_db = mongodb_db
         if not self.mongodb_uri:
-            sys.exit("Connection String is required!")
-            
+            self.mongodb_uri = self.remote_uri
+            print(self.mongodb_uri)
+            self.mongodb_db = self.init_db
+
+            # sys.exit("Connection String is required!")
+
     @classmethod
     def from_crawler(cls, crawler):
         return cls(
-            mongodb_uri = crawler.settings.get('MONGODB_URI'),
-            mongodb_db = crawler.settings.get('MONGODB_DATABASE', 'jobs')
+            mongodb_uri=crawler.settings.get('MONGODB_URI'),
+            mongodb_db=crawler.settings.get('MONGODB_DATABASE', 'jobs')
         )
-    
+
     def open_spider(self, spider):
         self.client = pymongo.MongoClient(self.mongodb_uri)
         self.db = self.client[self.mongodb_db]
-        
+
         # Start with a clean database
-        self.db[self.collection].delete_many({})
+        # self.db[self.collection].delete_many({})
 
     def close_spider(self, spider):
         self.client.close()
