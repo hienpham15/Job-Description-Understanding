41c34265c0815ee40fd65ad7ccb906d9815c3fbe
hienpham15 <phamxuanhien15@gmail.com>
2022-07-06 16:51:42 +0200

    Updated configurations to deploy on Heroku

---
+++ scrapBot/init.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 12:20:09 2022
+
+@author: hienpham
+"""
+import io
+import os
+from subprocess import Popen
+
+# https://devcenter.heroku.com/articles/runtime-principles#web-servers
+# The port to bind to is assigned by Heroku as the PORT environment variable.
+PORT = os.environ['PORT']
+with io.open("scrapyd.conf", 'r+', encoding='utf-8') as f:
+    f.read()
+    f.write(u'\nhttp_port = %s\n' % PORT)
+
+
+# Launch LogParser as a subprocess
+logs_dir = '/app/logs'
+if not os.path.exists(logs_dir):
+    os.mkdir(logs_dir)
+
+args = ['logparser', '-dir', logs_dir]
+args.extend(['--scrapyd_server', '127.0.0.1:%s' % PORT])
+args.extend(['--sleep', os.environ.get('PARSE_ROUND_INTERVAL', '10')])
+if os.environ.get('ENABLE_TELNET', 'True') == 'False':
+    args.extend(['--disable_telnet'])
+if os.environ.get('VERBOSE', 'False') == 'True':
+    args.extend(['--verbose'])
+Popen(args)
---
+++ scrapBot/jobs/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 14:10:22 2022
+
+@author: hienpham
+"""
